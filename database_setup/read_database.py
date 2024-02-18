99fe885f7e690c6b84935259e75eb5cec9cf38eb
Vinicius-O-Ferraz <146992032+Vinicius-O-Ferraz@users.noreply.github.com>
2024-02-17 23:31:22 -0300

    Update read_database.py

--- database_setup/read_database.py
+++ database_setup/read_database.py
@@ -1,7 +1,10 @@
 import pymysql
 
 
-def read_database():
+def takeAttributes(*attributes):
+    return attributes
+
+def read_database(attributes,table,condition):
     connection = pymysql.connect(
         host = "localhost",
         user = "root",
@@ -9,24 +12,20 @@ def read_database():
         database = "housing"
     )
     
-    attributes = input().split()
-    table = input()
-    condition = input()
-
-    print(','.join(attributes))
-
     sql = f""" SELECT {','.join(attributes)}
     FROM {table}
     WHERE {condition};
     """
     cursor = connection.cursor()
     cursor.execute(sql)
+
+
     result = cursor.fetchall()
-    
     for r in result:
         print(r)
 
     cursor.close()
     connection.close()
 
-read_database()
+attributes = takeAttributes('region', 'price' ,'houseType')
+read_database(attributes,'principal', 'price > 1000')
