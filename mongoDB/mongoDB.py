c2dda226fa211c5496dd04f5c6bde550d6c7989d
heitorleonny <heitorleonnyyt1234@gmail.com>
2024-02-19 23:17:08 -0300

    refactor: updater mongodb

--- mongoDB/mongoDB.py
+++ mongoDB/mongoDB.py
@@ -2,33 +2,22 @@
 # docker run --name mongo-container-2 vsgroot/rent-right-db
 
 
-import pymongo
-import docker
+import logging
+from pymongo import MongoClient
+from typing import Any, Dict, List, Optional
 
 class MongoDBManager:
-    _client = None
-    _db = None
+
+    # Configure the logger
+    logging.basicConfig(level=logging.DEBUG)
+    logger = logging.getLogger(__name__)
+
+    _client: MongoClient = MongoClient("mongodb://root:example@localhost:27017/rent-right?authSource=admin")
+    _db = _client.rent_right
     _collection = None
 
     @staticmethod
-    def _get_mongo_client():
-        if MongoDBManager._client is None:
-            client = docker.from_env()
-            container = client.containers.get('mongo-container-2')
-            container_info = container.attrs
-            networks = container_info['NetworkSettings']['Networks']
-            network_info = networks[list(networks.keys())[0]]  # Assuming only one network is used
-            container_ip = network_info['IPAddress']
-            MongoDBManager._client = pymongo.MongoClient(host=container_ip, port=27017,
-                                                        username='root', password='example',
-                                                        authSource='admin')
-        return MongoDBManager._client
-
-    @staticmethod
     def _get_database():
-        if MongoDBManager._db is None:
-            client = MongoDBManager._get_mongo_client()
-            MongoDBManager._db = client['rent-right']
         return MongoDBManager._db
 
     @staticmethod
@@ -37,24 +26,59 @@ class MongoDBManager:
             db = MongoDBManager._get_database()
             MongoDBManager._collection = db['rentRight']
         return MongoDBManager._collection
+    
+    @staticmethod
+    def insert_data(data: Dict[str, Any]) -> Optional[Any]:
+        """
+        Insert data into a collection.
+        """
+        try:
+            status = MongoDBManager._get_collection().insert_one(data)
+            MongoDBManager.logger.info(f'Data inserted successfully. ID: {status.inserted_id}')
+            return status.inserted_id
+        except Exception as e:
+            MongoDBManager.logger.error(f'Error inserting data: {str(e)}')
+            raise
 
     @staticmethod
-    def insert_document(document):
-        collection = MongoDBManager._get_collection()
-        result = collection.insert_one(document)
-        print("Document inserted with ID:", result.inserted_id)
+    def read_data() -> List[Dict[str, Any]]:
+        """
+        Read all documents in the collection.
+        """
+        try:
+            data = MongoDBManager._get_collection().find()
+            result = list(data)
+            MongoDBManager.logger.info(f'Reading successful. Total documents: {len(result)}')
+            return result
+        except Exception as e:
+            MongoDBManager.logger.error(f'Error reading data: {str(e)}')
+            raise
 
     @staticmethod
-    def update_document(query, update):
-        collection = MongoDBManager._get_collection()
-        result = collection.update_one(query, update)
-        print("Document updated:", result.modified_count)
+    def update_data(filter_query: Dict[str, Any], update_query: Dict[str, Any]) -> int:
+        """
+        Update documents in the collection based on a filter.
+        """
+        try:
+            status = MongoDBManager._get_collection().update_many(filter_query, update_query)
+            MongoDBManager.logger.info(f'Update successful. Modified documents: {status.modified_count}')
+            return status.modified_count
+        except Exception as e:
+            MongoDBManager.logger.error(f'Error updating data: {str(e)}')
+            raise
 
     @staticmethod
-    def delete_document(query):
-        collection = MongoDBManager._get_collection()
-        result = collection.delete_one(query)
-        print("Document deleted:", result.deleted_count)
+    def delete_data(filter_query: Dict[str, Any]) -> int:
+        """
+        Delete documents in the collection based on a filter.
+        """
+        try:
+            status = MongoDBManager._get_collection().delete_many(filter_query)
+            MongoDBManager.logger.info(f'Deletion successful. Deleted documents: {status.deleted_count}')
+            return status.deleted_count
+        except Exception as e:
+            MongoDBManager.logger.error(f'Error deleting data: {str(e)}')
+            raise
 
     @staticmethod
     def display_documents():
@@ -63,6 +87,30 @@ class MongoDBManager:
         for document in documents:
             print(document)
 
+"""
+# Exemplo de uso:
+banco = MongoDBManager()
+# Inserir dados
+sensor_data = {"id": 1, "e": 25, "humidity": 50}
+inserted_id = banco.insert_data(sensor_data)
+print("Documento inserido com ID:", inserted_id)
+
+# Ler dados
+data = banco.read_data()
+print("Dados na coleção:")
+for document in data:
+    print(document)
+
+# Atualizar dados
+filter_query = {"id": 1}
+update_query = {"$set": {"humidity": 60}}
+modified_count = banco.update_data(filter_query, update_query)
+print("Número de documentos atualizados:", modified_count)
+
+# Deletar dados
+filter_query = {"id": 1}
+deleted_count = banco.delete_data(filter_query)
+print("Número de documentos deletados:", deleted_count)
 
 # Inserindo um documento
 #MongoDBManager.insert_document({'region':'Camaragibe', 'price': 1500, 'houseType': 'apartment', 'sqFeet': 1200, 'beds': 3, 'baths': 1, 'catsAllowed':1, 'dogsAllowed':1, 'smokingAllowed':1, 'comesFurnished':1, 'latitude':81.4245, 'longitude': -23.4421,'state':'ca'})
@@ -75,3 +123,4 @@ class MongoDBManager:
          
 # Mostrando documentos
 MongoDBManager.display_documents()
+"""
